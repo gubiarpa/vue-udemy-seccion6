e24b5cfef07fb5d472ab942f6057f03ca8a3c6da
gubiarpa <billy.arredondo@pucp.pe>
2020-11-01 19:27:48 -0500

    Connect to API with Button

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,9 @@
 <template>
   <Titulo texto="Tîtulo de mi Blog" />
+  <button @click="consumirApi">Consumir API</button><br>
+  <select>
+    <option :value="item.id" v-for="item in arrayBlog" :key="item.id">{{ item.title }}</option>
+  </select>
 </template>
 
 <script>
@@ -7,6 +11,22 @@ import Titulo from "../components/Titulo";
 export default {
     components: {
         Titulo
+    },
+    data() {
+      return {
+        arrayBlog: []
+      }
+    },
+    methods: {
+      async consumirApi() {
+        try {
+          const data = await fetch('http://jsonplaceholder.typicode.com/posts'); // traemos de la API
+          const array = await data.json(); // configuramos
+          this.arrayBlog = array;
+        } catch (error) {
+          console.log(error);
+        }
+      }
     }
 }
 </script>
