149ab4a54e05dfe22f54097c49f085c3894ad8e1
gubiarpa <billy.arredondo@pucp.pe>
2020-11-01 19:58:33 -0500

    Dynamic Route implemented

--- src/views/Articulo.vue
+++ src/views/Articulo.vue
@@ -1,6 +1,8 @@
 <template>
   <Titulo texto="Ruta con parámetros" />
   <h2>{{ $route.params.id }}</h2>
+  <h3>{{ articulo.title }}</h3>
+  <p>{{ articulo.body }}</p>
 </template>
 
 <script>
@@ -8,6 +10,25 @@ import Titulo from "../components/Titulo";
 export default {
     components: {
         Titulo
+    },
+    data() {
+      return {
+        articulo: {}
+      }
+    },
+    methods: {
+      async consumirArticulo() {
+        try {
+          const data = await fetch(`http://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`);
+          const objeto = await data.json();
+          this.articulo = objeto;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+    created() {
+      this.consumirArticulo();
     }
 }
 </script>
